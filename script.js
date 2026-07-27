f61fa7182db9b4d815fac4016badf0e953e93acf
refullstudio <108417024+refullstudio@users.noreply.github.com>
2026-07-27 21:13:51 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const characters = [
     {
-        image: "characters/1.png",
+        image: "1.png",
         description: "A big yellow head :O"
     },
     {
-        image: "characters/2.png",
+        image: "2.png",
         description: "A guy with a bomb head crazy!"
     },
     {
-        image: "characters/3.png",
+        image: "3.png",
         description: "Music girl pink hair girl?"
     },
     {
-        image: "characters/4.png",
+        image: "4.png",
         description: "Pet."
     },
     {
@@ -20,63 +20,63 @@ const characters = [
         description: "Defender shooter flying bot thing?"
     },
     {
-        image: "characters/6.png",
+        image: "6.png",
         description: "A big head GREEN!"
     },
     {
-        image: "characters/7.png",
+        image: "7.png",
         description: "I'm not sure who picked her name but she is reflective!"
     },
     {
-        image: "characters/8.png",
+        image: "8.png",
         description: "Winter guy even tho it's CRAZY hot."
     },
     {
-        image: "characters/9.png",
+        image: "9.png",
         description: "The most least real."
     },
     {
-        image: "characters/10.png",
+        image: "10.png",
         description: "A gentleman ig?"
     },
     {
-        image: "characters/11.png",
+        image: "11.png",
         description: "Just a fat dude."
     },
     {
-        image: "characters/12.png",
+        image: "12.png",
         description: "The only thing you won't enjoy watching!"
     },
     {
-        image: "characters/13.png",
+        image: "13.png",
         description: "The BLUE version of a BIG head!"
     },
     {
-        image: "characters/14.png",
+        image: "14.png",
         description: "A weirdo with 4 arms."
     },
     {
-        image: "characters/15.png",
+        image: "15.png",
         description: "A dummy without an arm."
     },
     {
-        image: "characters/16.png",
+        image: "16.png",
         description: "A useless mouth."
     },
     {
-        image: "characters/17.png",
+        image: "17.png",
         description: "An actually useful guy!"
     },
     {
-        image: "characters/18.png",
+        image: "18.png",
         description: "Seriously don't mess with this guy..."
     },
     {
-        image: "characters/19.png",
+        image: "19.png",
         description: "THE BIGGER THE BETTER THE RED!"
     },
     {
-        image: "characters/20.png",
+        image: "20.png",
         description: "[REDACTED]"
     }
 ];
